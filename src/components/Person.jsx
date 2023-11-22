dd3eff0fc12df705e142f89dc01fa9fbe9e22c36
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-22 19:05:31 +0200

    handleRemove  props added

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,3 +1,5 @@
-const Person=({person})=><p>{person.name} {person.number}</p>
+const Person=({person,text,handleRemove})=><div>
+  <p>{person.name} {person.number} <button onClick={handleRemove}>{text}</button></p>
+</div>
 
 export default Person
