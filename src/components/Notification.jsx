206fb1703fae150f5cff233126471785b1c8879b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-22 20:20:16 +0200

    conditional if else to show notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,3 +1,11 @@
-const Notification=({message})=><p>{ message } </p>
+const Notification=({message})=>{
+  if(message.errormsg===null && message.successmsg===null){
+    return null
+  }else if(message.successmsg){
+    return<p>{message.successmsg}</p>
+  }else{
+    return<p>{message.errormsg}</p>
+  }
+}
 
 export default Notification;
