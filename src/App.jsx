fb9c6fc407b83f3c20e2e351656b40ec0afe2a7a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-22 17:16:18 +0200

    importing person and contactform component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import { useState } from 'react'
+import ContactForm from './components/ContactForm'
+import Person from './components/Person'
 
 const App=()=> {
-  //const [count, setCount] = useState(0)
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const handleName=(e)=>setNewName(e.target.value)
+  const handleNumber=(e)=>setNewNumber(e.target.value)
+  
+  const handleSubmit=(e)=>{
+    console.log(e.target);
+  }
+
+
 
   return (
-    <p>Contact book App</p>  
+    <>
+      <h2>ContactBook</h2>
+      <ContactForm name={newName}  number={newNumber} 
+        handleName={handleName} handleNumber={handleNumber}
+        handleSubmit={handleSubmit}/>
+        <h2>Numbers</h2>
+      { persons.map(person=><Person key={person.id} person={person}/>)
+      }
+    </>  
   )
 }
 
