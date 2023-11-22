118314ffd253ebb53d0fb1ec95c176a5363344cf
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-22 19:06:37 +0200

    adding submit botton

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-const ContactForm=({handleName,handleNumber,handleSubmit,name,number})=><form>
+const ContactForm=({handleName,handleNumber,handleSubmit,name,number})=><form onSubmit={handleSubmit}>
   <div>
     <label>Name</label><input type="text" value={name} onChange={handleName}/><br/>
     <label>Tel.no</label><input type="number" value={number} onChange={handleNumber}/>
