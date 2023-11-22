09e5b332ec2a4ceb487de533d3f0bae0898726ec
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-22 17:14:18 +0200

    Adding person and contact form component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,13 @@
+const ContactForm=({handleName,handleNumber,handleSubmit,name,number})=><form>
+  <div>
+    <label>Name</label><input type="text" value={name} onChange={handleName}/><br/>
+    <label>Tel.no</label><input type="number" value={number} onChange={handleNumber}/>
+  </div>
+  <div>
+    <button type="submit">Add</button>
+  </div>
+
+</form>
+
+
+export default ContactForm;
