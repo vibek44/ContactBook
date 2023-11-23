b88e40b811a07ab0b2431d9c42dc518ccb76f59a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-23 15:09:49 +0200

    Changing return value of component to tr element

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,5 +1,9 @@
-const Person=({person,text,handleRemove})=>
-  <p>{person.name} {person.number} <button onClick={handleRemove}>{text}</button></p>
-
+const Person=({person,text,handleRemove})=><tr>
+    <td>{person.name} </td>
+    <td>{person.number}</td>
+    <td><button type="button" onClick={handleRemove}>{text}</button></td>
+   
+   </tr>
+   
 
 export default Person
