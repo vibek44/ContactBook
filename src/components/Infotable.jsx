4e6b401b9dea5cc3802d2441076708b45b2aeeb2
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 19:15:58 +0200

    adding prop for person component

--- src/components/Infotable.jsx
+++ src/components/Infotable.jsx
@@ -1,6 +1,6 @@
 import Person from "./Person"
 
-const Infotable=({peoples,handleRemove})=><table>
+const Infotable=({peoples,handleRemove,handleEdit})=><table>
 <caption>Phone book</caption>
 <tbody>
  <tr>
@@ -9,7 +9,8 @@ const Infotable=({peoples,handleRemove})=><table>
    <th>Action</th>
  </tr> 
  { peoples.map( person=><Person key={person.id} person={person}
-     handleRemove={()=>handleRemove(person.id)} text="delete"/> ) 
+     handleRemove={()=>handleRemove(person.id)} handleEdit={()=>handleEdit(person.id)}
+     text1="delete" text2="edit"/> ) 
  }
 </tbody>
 </table>
