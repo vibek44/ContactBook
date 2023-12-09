b849f26133392a72e3b039a8b8c939962393941f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 19:16:49 +0200

    adding edit button

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,9 +1,17 @@
-const Person=({person,text,handleRemove})=><tr>
+const Person=({person,text1,text2,handleRemove,handleEdit})=>{
+    const buttonStyle = {
+       marginRight:5 
+      }
+return(<tr>
     <td>{person.name} </td>
     <td>{person.number}</td>
-    <td><button type="button" onClick={handleRemove}>{text}</button></td>
+    <td ><button style={buttonStyle} type="button" onClick={handleRemove}>{text1}</button>
+        <button type="button" onClick={handleEdit}>{text2}</button>
+    </td>
+
    
-   </tr>
+   </tr>)
+}
    
 
 export default Person
