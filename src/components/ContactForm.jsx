385cf8fc04bee9a3248ab76d00937aec12d9615f
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 19:14:11 +0200

    changing input type to tel

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,24 @@
 
 const ContactForm=({handleName,handleNumber,handleSubmit,name,number})=> <form  className="form-container" onSubmit={handleSubmit}>
-                              <fieldset> 
-                                <legend>Add contact</legend>
-                                <div>
-                                    Name: 
-                                    <input   value={name} 
-                                    onChange={handleName} />
-                                </div>
-                                <div>
-                                Number: 
-                                <input  type="number" 
-                                    value={number} 
-                                    onChange={handleNumber} />
-                                </div>
-                              
-                                <button type="submit"  >add</button>
-                               
-                              </fieldset>
-                          </form>
+    <fieldset> 
+        <legend>Add contact</legend>
+        <div>
+            Name: 
+            <input   value={name} 
+            onChange={handleName} />
+        </div>
+        <div>
+            Number: 
+            <input type="tel" 
+                value={number} 
+                onChange={handleNumber} 
+                pattern="[0-9]{2,3}-[0-9]{7,8}"/>
+        </div>
+
+        <button type="submit"  >add</button>
+    
+    </fieldset>
+</form>
 
 
 
