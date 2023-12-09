be4dd3e4fe6b21b779149c657cff003256ccbd23
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 19:18:24 +0200

    modifying url to relative url

--- src/services/Communication.jsx
+++ src/services/Communication.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseurl=`http://localhost:3001/persons`
+const baseurl='/api/persons'
 
 const getAll=()=>{
   const request=axios.get(baseurl)
@@ -28,3 +28,4 @@ export default {
   update,
   remove
 }
+
